c3b63abaf08efd99e1231e4964fcb56d91801ae5
mites-vasoya <mrvasoya2001@gmail.com>
2023-07-03 01:33:51 +0530

    Front-End Header, table Created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import './App.css';
+import EmployeesDataTable from "./components/EmployeesDataTable";
+import {Button} from "@mui/material";
+import AddIcon from '@mui/icons-material/Add';
 
 function App() {
     return (
-        <div className="App">
-            
+        <div className="app">
+            <div className="header">
+                <p>AimBrill Employee Data</p>
+            </div>
+            <div className="tableDiv">
+                <div className="headerDiv">
+                    <Button variant="contained" sx={{backgroundColor: "black"}} startIcon={<AddIcon/>}
+                            className="newDataBtn"> Add New
+                        Data</Button>
+                </div>
+                <EmployeesDataTable/>
+            </div>
+            <div className="footer">
+                
+            </div>
         </div>
     );
 }
