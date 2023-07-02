3fb927f087e6f2e2d0c9309972eb5209c20ae662
mites-vasoya <mrvasoya2001@gmail.com>
2023-07-02 09:21:14 +0530

    File Import and Data Reading ✔️

--- server/routes/fileImport.route.js
+++ server/routes/fileImport.route.js
@@ -13,17 +13,41 @@ const storage = multer.diskStorage({
 });
 const upload = multer({storage: storage});
 
+const pool = require("../db/db");
+const {insertImportedFileData} = require("../crud_ops/crud_ops");
+
 
 //File import route
-router.post("/import", upload.single('file'), (req, res) => {
+router.post("/import", upload.single('file'), async (req, res) => {
     const importedFile = req.file;
-    console.log("IMPORTED FILES : ", importedFile);
 
-    const sheetResponse = readExcelController(importedFile.path);
+    //Read Data from the excel file
+    const excelParsedData = readExcelController(importedFile.path);
+    const tableName = 'employee_data';
+    const columnNames = Object.keys(excelParsedData[0]);
 
-    console.log("FILE DATA : ", sheetResponse)
+    //Convert Column Name into lowercase
+    columnNames.map((columnName, i) => {
+        columnNames[i] = columnName.toLowerCase();
+    })
 
+    //Query to create new table
+    const createTableQuery = `CREATE TABLE ${tableName} (${columnNames.map(name => `"${name}" TEXT`).join(', ')})`;
+
+    //Create table and insert data
+    const createTable = await pool.query(createTableQuery, (err, result) => {
+        if (err) {
+            console.error('Error creating table', err);
+            return;
+        } else {
+            console.log('Table created successfully');
+            insertImportedFileData(excelParsedData, tableName);
+        }
+    });
+
+    //Return response to the client
     res.status(200).json(importedFile);
 });
+
 
 module.exports = router;
